87d739ae4550d2b056324fa267ea65feae3a8f2e
suyashsahu3 <suyash.sahu_cs19@gla.ac.in>
2022-05-09 15:25:15 +0530

    commit

--- Assets/parallax/pass.js
+++ Assets/parallax/pass.js
@@ -1,6 +1,6 @@
 
 
-var password = "QwertySuyash";
+var password = "Suy@Sam";
 var x = prompt("Enter in the password "," ");
 if (x == password) {
  alert("Come right in \n \n You've entered in the right password");
