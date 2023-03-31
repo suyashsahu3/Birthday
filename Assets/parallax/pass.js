64df93476b4c3f04a4d670595a2d34f126009d84
Suyash Sahu <suyashsahu3@gmail.com>
2023-04-01 01:03:09 +0530

    fix: password alert

--- Assets/parallax/pass.js
+++ Assets/parallax/pass.js
@@ -1,7 +1,7 @@
 
 
 var password = "Suy@Sam";
-var x = prompt("Enter in the password!!);
+var x = prompt("Enter in the password!!");
 if (x == password) {
  alert("Come right in \n \n You've entered in the right password");
  window.location = "birthday.html";
